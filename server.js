ce879556bb0be64d849e2d80b9eb27ab0c508861
EasyHub Global <scribesoul.a@gmail.com>
2025-11-14 01:13:50 +0000

    Fix CORS issue

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "http://localhost:3001",
-    "https://anjola-frontend.vercel.app"
+    "https://anjola-frontend.vercel.app",
+    "http://localhost:3000"
   ],
-  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
 
 app.use(express.json());
