a7be46036ce9bec82f60dd46cea571d5d6f3aa38
EasyHub Global <scribesoul.a@gmail.com>
2025-12-21 21:36:06 +0000

    Update

--- services/paystackService.js
+++ services/paystackService.js
@@ -8,8 +8,8 @@ exports.initializePayment = async (email, amount, metadata) => {
     metadata,
     // Redirect user back to your site after payment
     callback_url: process.env.FRONTEND_URL 
-      ? `${process.env.FRONTEND_URL}/payment-success`
-      : 'https://anjolaaestheticsng.com/payment-success'
+      ? `${process.env.FRONTEND_URL}/checkout-complete`
+      : 'https://anjolaaestheticsng.com/checkout-complete'
   });
 
   const options = {
